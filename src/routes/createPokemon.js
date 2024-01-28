0b720de3e407235a05976b79239f709d9a8077bc
vectokse <nicolas.schlaflang@gmail.com>
2024-01-28 22:39:04 +0100

    Pokemon.js : Implements getter and setter for automatic conversion of 'types' fields

--- src/routes/createPokemon.js
+++ src/routes/createPokemon.js
@@ -2,14 +2,7 @@ const { Pokemon } = require('../db/sequelize')
   
 module.exports = (app) => {
     app.post('/api/pokemons', (req, res) => {
-        const pokemonCreated = req.body
-        Pokemon.create({
-            name: pokemonCreated.name,
-            hp: pokemonCreated.hp,
-            cp: pokemonCreated.cp,
-            picture: pokemonCreated.picture,
-            types: pokemonCreated.types.join()
-        })
+        Pokemon.create(req.body)
         .then(pokemon => {
             const message = `Le pokémon ${req.body.name} a bien été crée.`
             res.json({ message, data: pokemon })
